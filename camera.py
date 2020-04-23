b76a30cc0cd79393afb9cc1be4bcac0e43248f0a
Matt Boran <mattboran@gmail.com>
2020-04-23 15:35:02 -0400

    Add comments, cleanup

--- camera.py
+++ camera.py
@@ -6,45 +6,40 @@ import cv2
 from PIL import ImageFont, ImageDraw, Image
 
 
-chars = np.asarray(list(' .,:irs?@9B&#'))
-char_map = {i: e for i, e in enumerate(chars)}
-
 def frame_to_chars(frame):
-    # Our operations on the frame come here
+    chars = np.asarray(list(' .,:irs?@9B&#'))
+    char_map = {i: e for i, e in enumerate(chars)}
+    # Convert to black and white, make low res
     bw_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     low_res = cv2.resize(bw_frame, (80, 40))
     low_res = cv2.flip(low_res, 1).astype(np.uint8)
-
     char_indices = (low_res / 255 * (chars.size - 1)).astype(np.uint8)
+    # Convert ints to characters using the char_map
     as_chars = np.vectorize(char_map.get)(char_indices)
     return "\n".join(["".join(row) for row in as_chars])
 
 def main():
+    # Capture from /dev/video0
     cap = cv2.VideoCapture(0)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 
     font = ImageFont.truetype("./VeraMono.ttf", 10)
     while(True):
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-
-        text = frame_to_chars(frame)
-
-        pil_img = Image.fromarray(np.zeros((600, 600)))
-
-        draw = ImageDraw.Draw(pil_img)
-        draw.text((0, 0), text, font=font)
-        ascii_img = np.array(pil_img)
-
-        # Display the resulting frame
-        cv2.imshow('ascii', ascii_img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        try:
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+            text = frame_to_chars(frame)
+            # Write to PIL image
+            pil_img = Image.fromarray(np.zeros((600, 600)))
+            draw = ImageDraw.Draw(pil_img)
+            draw.text((0, 0), text, font=font)
+            # Convert back to np array and write to stdout
+            uint_img = (np.array(pil_img) * 255).astype(np.uint8)
+            sys.stdout.buffer.write(uint_img.tostring())
+        except KeyboardInterrupt:
             break
-
-    # When everything done, release the capture
     cap.release()
-    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
